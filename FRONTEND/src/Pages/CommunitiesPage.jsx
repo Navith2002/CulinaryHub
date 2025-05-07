c32b8dd55e94bb04d5e67ea067297b8f00a1f4e5
SupuniNishakya <supuninishakya01@gmail.com>
2025-05-07 23:19:10 +0530

    fix errors
    fix

--- FRONTEND/src/Pages/CommunitiesPage.jsx
+++ FRONTEND/src/Pages/CommunitiesPage.jsx
@@ -164,6 +164,7 @@ const CommunitiesPage = () => {
                     ? "bg-blue-600 text-white"
                     : "bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300"
                 }`}
+                //  //  
               >
                 Public
               </button>
